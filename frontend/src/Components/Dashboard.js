a218ede4a41f43198b50ab8765cc81aea9427580
ameybhosle1 <ameybhosle3@gmail.com>
2021-10-18 12:55:34 +0530

    Profile Issue Solved

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -6,12 +6,6 @@ import Navs from "./Nav";
 import Posts from "./Posts";
 
 const Dashboard = ()=>{
-    const [posts , setPosts] = useState({});
-    const [currentPage , setCurrentPage] = useState(1);
-    const [postsPerPage , setPostsPerPage] = useState(10);
-    const indexOfLastPage = currentPage * postsPerPage;
-    const indexOfFirstPage = indexOfLastPage - postsPerPage;
-
     useEffect(()=>{
         async function datacall(){
             var {data} = await axios.post("http://localhost:1001/recipe/all",{'token':localStorage.getItem('user')})
@@ -20,6 +14,11 @@ const Dashboard = ()=>{
         }
         datacall()
     },[])
+    const [posts , setPosts] = useState({});
+    const [currentPage , setCurrentPage] = useState(1);
+    const [postsPerPage , setPostsPerPage] = useState(10);
+    const indexOfLastPage = currentPage * postsPerPage;
+    const indexOfFirstPage = indexOfLastPage - postsPerPage;
     return (
         <Context.Provider value={{data:posts}}>
             <Navs />
